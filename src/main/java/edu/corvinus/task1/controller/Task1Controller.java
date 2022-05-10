8b07cd8a47c227c19e74480c87b5f817419b068e
corvinusdk8b2n <balazsadany@gmail.com>
2022-05-10 11:45:14 +0200

    IC

---
+++ src/main/java/edu/corvinus/task1/controller/Task1Controller.java
@@ -0,0 +1,33 @@
+package edu.corvinus.task1.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RestController
+public class Task1Controller
+{
+    @RequestMapping("/")
+    public String veletlen(@RequestParam(value ="number") String numberString)
+    {
+        double number = Double.parseDouble(numberString);
+        double newNumber = 0;
+        String result = "";
+
+        if(number >= 0)
+        {
+            newNumber = Math.sqrt(number);
+            result = Double.toString(newNumber);
+        }
+        else
+        {
+            double abs = Math.abs(number);
+            abs = Math.sqrt(abs);
+            result = abs + "i";
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/edu/corvinus/task1/Task1ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.corvinus.task1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
